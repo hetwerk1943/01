fbffdfd2087f3752e80a944be0c6c34487f82533
AlgoMaster <hetwerk1943@gmail.com>
2026-03-01 16:45:14 +0100

    Initialize copilot/run-all-linters

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["web/**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "script",
+      globals: {
+        ...globals.browser,
+        Chart: "readonly",
+      },
+    },
+  },
+];
